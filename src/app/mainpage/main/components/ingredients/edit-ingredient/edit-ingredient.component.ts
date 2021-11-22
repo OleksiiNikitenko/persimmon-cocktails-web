9cf9ced05def76f5d12c80a4bd38c9a1b7c48aa0
Tsvik Roman <35147356+Roman1236@users.noreply.github.com>
2021-11-22 14:24:51 +0200

    added edit ingredient form

---
+++ src/app/mainpage/main/components/ingredients/edit-ingredient/edit-ingredient.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+
+
+@Component({
+  selector: 'app-edit-ingredient',
+  templateUrl: './edit-ingredient.component.html',
+  styleUrls: ['./edit-ingredient.component.css', '../../../../../app.component.css']
+})
+export class EditIngredientComponent implements OnInit {
+
+  form: FormGroup
+  selectedFile: File = null as any;
+
+
+  constructor(
+    private formBuilder: FormBuilder, private http: HttpClient
+  ) {
+    this.form = this.formBuilder.group({
+      name: '',
+      category: ''
+    })
+  }
+
+  ngOnInit(): void {}
+
+  get name() {
+    return this.form.get('name')
+  }
+
+  get category() {
+    return this.form.get('category')
+  }
+
+  resetFormHandler() {
+    this.name?.setValue('')
+    this.category?.setValue('')
+  }
+}
