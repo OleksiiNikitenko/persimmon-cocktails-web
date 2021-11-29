143f4cb44d691755555e4aa3d6d2a4cfb86d94cf
kirill26031 <kirill26031@gmail.com>
2021-11-29 13:38:40 +0200

    Interceptor for authorization works

--- src/app/utils/interceptor.ts
+++ src/app/utils/interceptor.ts
@@ -15,35 +15,36 @@ export class Interceptor implements HttpInterceptor {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (this.accessUserStorage.isPresent()) {
-      let token = this.accessUserStorage.get()?.token
+    if (this.accessUserStorage.isPresent() && this.isOurDomain(req)) {
+      let token: string | undefined = this.accessUserStorage.get()?.token
+      if (token == undefined) throw "stored accessUser should not be null"
 
       //how to update the request Parameters
       const updatedRequest = req.clone({
-        headers: req.headers.set("Authorization", "Some-dummyCode")
+        headers: req.headers.set("Authorization", token)
       });
       //logging the updated Parameters to browser's console
-      console.log("Before making api call : ", updatedRequest);
-      return next.handle(req).pipe(
+      // console.log("Before making api call : ", updatedRequest);
+      return next.handle(updatedRequest).pipe(
         tap(
           event => {
             //logging the http response to browser's console in case of a success
-            if (event instanceof HttpResponse) {
-              console.log("api call success :", event);
-            }
+            // if (event instanceof HttpResponse) {
+            //   console.log("api call success :", event);
+            // }
           },
           error => {
             //logging the http response to browser's console in case of a failuer
-            if (event instanceof HttpResponse) {
-              console.log("api call error :", event);
-            }
+            // if (event instanceof HttpResponse) {
+            //   console.log("api call error :", event);
+            // }
           }
         )
       );
-    } else {
-      console.log("authorisation is not added for " + req.url);
-      return next.handle(req)
     }
+    console.log("authorisation is not added for " + req.url);
+    return next.handle(req)
+
   }
 
   isOurDomain(req: HttpRequest<any>): boolean {
