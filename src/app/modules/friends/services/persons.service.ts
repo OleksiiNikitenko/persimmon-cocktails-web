5a8ba488ae24f7b67752b63d8f267c13ebecc200
nastya_nskkk <nastyakovalenko25.04.2001@gmail.com>
2021-12-15 14:31:18 +0200

    image service

--- src/app/modules/friends/services/persons.service.ts
+++ src/app/modules/friends/services/persons.service.ts
@@ -1,9 +1,8 @@
 import {Injectable} from "@angular/core";
 import {Observable} from 'rxjs';
 import {HttpClient} from "@angular/common/http";
-import {environment} from "../../../../environments/environment";
-import {FriendModel} from "../models/friend.model";
 import {FoundUsersModel} from "../models/found-users.model";
+import {URLS} from "../../../core/models/urls";
 
 
 @Injectable({
@@ -11,18 +10,16 @@ import {FoundUsersModel} from "../models/found-users.model";
 })
 
 export class PersonsService {
-  private apiServerUrl = environment.apiBaseUrl;
-  private personsSearchUrl = `${this.apiServerUrl}/person/search/`
-  private personsPagesAmountUrl = `${this.apiServerUrl}/person/search-pages-number/`
+  urls =URLS
 
   constructor(private http: HttpClient) {
   }
 
   getPersonsByName(name: string, page: number): Observable<FoundUsersModel[]> {
-    return this.http.get<FoundUsersModel[]>(this.personsSearchUrl + name + '?page=' + page);
+    return this.http.get<FoundUsersModel[]>(this.urls.personsSearchUrl + name + '?page=' + page);
   }
 
   getPagesAmount(name: string): Observable<number> {
-    return this.http.get<number>(this.personsPagesAmountUrl + name);
+    return this.http.get<number>(this.urls.personsPagesAmountUrl + name);
   }
 }
--- src/app/modules/settings/components/settings.component.ts
+++ src/app/modules/settings/components/settings.component.ts
@@ -4,6 +4,11 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {UserQuery} from "../../account/services/user-query";
 import {User} from "../../account/models/user-model";
 import {UserService} from "../../account/services/user-service";
+import {ImageModel} from "../../image/model/image.model";
+import {ImageUploadService} from "../../image/services/image-upload-service";
+import {AccountComponent} from "../../account/components/account.component";
+import {HttpErrorResponse} from "@angular/common/http";
+
 
 @Component({
   selector: 'app-settings',
@@ -14,14 +19,19 @@ import {UserService} from "../../account/services/user-service";
 export class SettingsComponent implements OnInit {
   person: User | undefined;
   form: FormGroup
+  imageUrl: any;
+  imageNotAvailable = '../../../../assets/images/user.png';
+  loading: boolean = false;
+  file: File | undefined;
 
   constructor(
     public dialogChangePass: ChangePasswordComponent,
-    // private settingsService: SettingsService,
     private formBuilder: FormBuilder,
     private userService: UserService,
     private userQuery: UserQuery,
     private cdr: ChangeDetectorRef,
+    private imageService: ImageUploadService,
+    private accountComponent: AccountComponent,
   ) {
     this.form = this.formBuilder.group({
       name: ['', Validators.required]
@@ -37,10 +47,51 @@ export class SettingsComponent implements OnInit {
       this.person = person
       this.name?.setValue(person.name)
       this.cdr.markForCheck()
+      // this.accountComponent.getImageById(person.photoId)
+      this.getImageById(person.photoId)
     })
   }
 
   renameUser() {
     this.userService.updateUserName(this.form.value)
+  }
+
+  public getImageById(imageId: number) {
+    if (imageId != null) {
+      this.imageService.getImageById(imageId).subscribe(
+        (response) => {
+          if (response != null)
+            this.imageUrl = response.urlMiddle
+          else
+            this.imageUrl = this.imageNotAvailable
+        },
+        (error: HttpErrorResponse) => {
+          throw error;
+        }
+      );
+    }
+  }
+
+  onChange(event: any) {
+
+    this.file = event.target.files[0];
+  }
+
+  onUpload() {
+    if (this.file != undefined) {
+      this.loading = !this.loading;
+      console.log(this.file);
+      this.imageService.upload(this.file).subscribe(
+        (event: ImageModel) => {
+          if (typeof (event) === 'object') {
+            this.loading = false;
+            console.log(event)
+            this.accountComponent.getImageById(event.imageId);
+            this.userService.updatePhoto(event.imageId);
+            window.location.reload();
+          }
+        }
+      );
+    }
   }
 }
