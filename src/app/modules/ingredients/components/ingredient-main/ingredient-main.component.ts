b9b3b6fa051641225267e34c9d2d05e3327e965b
mechanoid7 <mechanis1@gmail.com>
2021-12-20 12:01:51 +0200

    fix status button update

--- src/app/modules/ingredients/components/ingredient-main/ingredient-main.component.ts
+++ src/app/modules/ingredients/components/ingredient-main/ingredient-main.component.ts
@@ -27,7 +27,7 @@ export class IngredientMainComponent implements AfterViewInit, OnInit {
   displayedColumns: string[] = ['photoId', 'name', 'category', 'dynamicButton', 'statusButton'];
   canEdit: boolean = getUser().role === Roles.Moderator || getUser().role === Roles.Admin
   ingredients: Ingredient[] = [];
-  ingredientsId : IngredientIdModel[] = [];
+  ingredientsId: IngredientIdModel[] = [];
   dataSource: any;
   searchIngredientsForm: FormGroup | any;
   searchIngredientsRequest: string = "";
@@ -36,7 +36,7 @@ export class IngredientMainComponent implements AfterViewInit, OnInit {
   imageNotAvailable = 'https://i.ibb.co/16mJRVD/67eb9e144841.jpg'
   @ViewChild(MatSort, {static: false}) sort!: MatSort;
   toggle = true;
-  statusBtn:string[] = [];
+  statusBtn: string[] = [];
   status: boolean[] = []
   buttonAddEnabled: boolean[] = []
 
@@ -45,10 +45,11 @@ export class IngredientMainComponent implements AfterViewInit, OnInit {
               private ingredientsQuery: IngredientsQuery,
               private ingredientsStore: IngredientsStore,
               private cdr: ChangeDetectorRef,
-              private imageService: ImageUploadService) {}
+              private imageService: ImageUploadService) {
+  }
 
 
-  getIngredients(): Ingredient[]{
+  getIngredients(): Ingredient[] {
     this.handleIngredientStatus()
     return this.ingredients;
   }
@@ -69,20 +70,20 @@ export class IngredientMainComponent implements AfterViewInit, OnInit {
     this.ingredientsQuery.selectAll().pipe(
       untilDestroyed(this)
     ).subscribe(ingredients => {
-        this.ingredients = ingredients
-        this.dataSource = new MatTableDataSource(ingredients)
-        this.dataSource.sort = this.sort;
-        this.cdr.markForCheck()
+      this.ingredients = ingredients
+      this.dataSource = new MatTableDataSource(ingredients)
+      this.dataSource.sort = this.sort;
+      this.cdr.markForCheck()
       console.log(ingredients)
-        this.imagesUrl = Array(ingredients.length).fill(this.imageNotAvailable)
-        this.setImages(ingredients)
-        this.statusBtn=Array(ingredients.length).fill('Enabled')
-
+      this.imagesUrl = Array(ingredients.length).fill(this.imageNotAvailable)
+      this.setImages(ingredients)
+      this.statusBtn = Array(ingredients.length).fill('Enabled')
+      this.getStockIngredientsId()
       this.handleIngredientStatus()
     })
   }
 
-  getIngredientFromDB(): void{
+  getIngredientFromDB(): void {
     this.ingredientsService.fetchIngredients()
     this.ingredientsQuery.selectAll().pipe(
       untilDestroyed(this)
@@ -91,7 +92,7 @@ export class IngredientMainComponent implements AfterViewInit, OnInit {
       this.dataSource = new MatTableDataSource(ingredients)
       this.dataSource.sort = this.sort;
       this.cdr.markForCheck()
-      this.statusBtn=Array(ingredients.length).fill('Enabled')
+      this.statusBtn = Array(ingredients.length).fill('Enabled')
       this.handleIngredientStatus()
       this.getStockIngredientsId();
     })
@@ -102,15 +103,15 @@ export class IngredientMainComponent implements AfterViewInit, OnInit {
       event.code : event.preventDefault();
   }
 
-  handleIngredientStatus(){
+  handleIngredientStatus() {
     for (let i = 0; i < this.ingredients.length; i++) {
-      if (!this.ingredients[i].active){
-        this.statusBtn[i]='Disabled';
-      }
-      else  this.statusBtn[i]='Enabled';
+      if (!this.ingredients[i].active) {
+        this.statusBtn[i] = 'Disabled';
+      } else this.statusBtn[i] = 'Enabled';
 
     }
   }
+
   getImageByIdIngredients(imageId: number, i: number) {
     if (imageId != null) {
       this.imageService.getImageById(imageId).subscribe(
@@ -127,44 +128,44 @@ export class IngredientMainComponent implements AfterViewInit, OnInit {
       );
     }
   }
+
   setImages(ingredients: any) {
     for (let i = 0; i < ingredients.length; i++) {
       this.getImageByIdIngredients(ingredients[i].photoId, i)
     }
   }
+
   ngAfterViewInit(): void {
   }
 
-  addToStock(ingredientId : number) {
-    this.ingredientsService.addToStock(ingredientId).subscribe((response) => {})
+  addToStock(ingredientId: number) {
+    this.ingredientsService.addToStock(ingredientId).subscribe((response) => {
+    })
   }
 
-  public changeStatus(ingredientId:number, index: number) {
+  public changeStatus(ingredientId: number, index: number) {
     console.log(this.ingredients)
-    if(this.ingredients.length != 0)
-    {
-      this.ingredientsService.changeStatus(ingredientId, this.ingredients[index].active).pipe(first()).subscribe((response: any)=>{
+    if (this.ingredients.length != 0) {
+      this.ingredientsService.changeStatus(ingredientId, this.ingredients[index].active).pipe(first()).subscribe((response: any) => {
         this.getIngredientFromDB()
       })
-      if (this.statusBtn[index]=='Enabled') {
-        this.statusBtn[index] ='Disabled';
-      }
-      else {
+      if (this.statusBtn[index] == 'Enabled') {
+        this.statusBtn[index] = 'Disabled';
+      } else {
         this.statusBtn[index] = 'Enabled';
       }
     }
   }
 
   getIngredientByName(name: string): void {
-    if (this.searchIngredientsForm.valid && name!='') {
+    if (this.searchIngredientsForm.valid && name != '') {
       this.ingredientsService
         .searchIngredient(name)
         .subscribe((ingredients: Ingredient[]) => {
           this.ingredients = ingredients;
           this.dataSource = new MatTableDataSource(this.ingredients);
         });
-    }
-    else{
+    } else {
       this.getIngredientFromDB()
     }
   }
@@ -183,14 +184,13 @@ export class IngredientMainComponent implements AfterViewInit, OnInit {
         this.ingredientsId = ingredientsId;
         for (let i = 0; i < this.ingredients.length; i++) {
           for (let j = 0; j < this.ingredientsId.length; j++) {
-            if(this.ingredients[i].ingredientId==this.ingredientsId[j].ingredientId) {
+            if (this.ingredients[i].ingredientId == this.ingredientsId[j].ingredientId) {
               this.buttonAddEnabled[i] = false;
             }
           }
         }
       }
     )
-
   }
 }
 
